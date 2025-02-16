0f394c833fddbc11b167d902e62138aa4d706e86
Mohit <bluesky5april2021@gmail.com>
2025-02-16 17:11:34 +0530

    minor changes

--- src/app/showcase/showcase.component.ts
+++ src/app/showcase/showcase.component.ts
@@ -1,17 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
 import projects from '../../../public/projects.json'
 
 @Component({
   selector: 'app-showcase',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './showcase.component.html',
   styleUrl: './showcase.component.css'
 })
 export class ShowcaseComponent implements OnInit {
   projects_list = signal(false)
-
+  project_categories = []
   ngOnInit(): void {
+    this.project_categories = [
+      {
+        "image_link":"project_icons/complete.svg"
+      },
+      {
+        "image_link":"project_icons/ml.svg"
+      },
+      {
+        "image_link":"project_icons/programming.svg"
+      },
+      {
+        "image_link":"project_icons/web_dev.svg"
+      }
+    ]
     this.project = projects;
     this.projectitem = projects[0]
   }
@@ -32,5 +48,7 @@ export class ShowcaseComponent implements OnInit {
     this.currentindex.update(index => index === 0?total :(index-1)%total)
     this.projectitem = this.project[this.currentindex()]
   }
+
+  
   list = [10,23, "ngrx", "heritics","sigma","alpha","asdasda"]
 }
